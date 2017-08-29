9b0262999eda2b915b083a923e5071681b1631e8
Richard Paterson <rocketbootkid@gmail.com>
2017-08-29 22:54:55 +0100

    Update

--- background.js
+++ background.js
@@ -19,29 +19,39 @@ var context = canvas.getContext('2d');
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
 
-var canvasDimensions = {
+var canvas = {
 		width: window.innerWidth,
-		mid_x: window.innerWidth/2,
 		mid_y: window.innerHeight/2,
 		height: window.innerHeight
 	};
 
 var segmentLength = 18; // How long each segment of a horison is
-var segmentCount = Math.ceil(canvasDimensions.width/segmentLength); // Determine how many segments fit on screen
+var segmentCount = Math.ceil(canvas.width/segmentLength); // Determine how many segments fit on screen
 var horizonCount = 12;
-var horizonStartY = canvasDimensions.mid_y;
-var staticHorizonStartY = canvasDimensions.mid_y;
+var horizonStartY = canvas.mid_y;
+var staticHorizonStartY = canvas.mid_y;
 var horizonStartX = 0;
 var horizonChangeMax = 15; // Amount by which points on horizon can vary +/- vertically
 var horizonDeltaY = 10; // Amount by which different horizons differ vertically
 var horizonSmootheningFactor = 4; // Amount by which nearer horizons become smoother
 var horizonNeareningFactor = 1; // Amount by which nearer horizons get lower
 
+var numBirds = 7;
+var birdRectangle = {
+	x: 200,
+	y: 100
+}
+
 drawSky();
 
 for (h = 0; h < horizonCount; h++) {
 	//console.log("Horizon " + h + " Start Y: " + horizonStartY);
 	drawHorizon();
+	
+	if (h > (horizonCount - 3)) {
+		console.log("Birds!");
+		
+	}
 	
 	// Set / reset parameters for next horizon
 	horizonStartY = staticHorizonStartY + (h * horizonDeltaY) + (Math.ceil(Math.random()*2*horizonDeltaY)) - horizonDeltaY; // Make next horizon lower down
@@ -51,9 +61,6 @@ for (h = 0; h < horizonCount; h++) {
 
 }
 
-drawTitle("External Platform Software");
-drawDescription("Software components and configurations that are not a part of the shipping product, but are required (or optional) in order for the product to work: operating systems, concurrently executing applications, drivers, fonts, etc.");
-
 // ******************************************************************************
 //                              FUNCTIONS
 // ******************************************************************************
@@ -61,12 +68,13 @@ drawDescription("Software components and configurations that are not a part of t
 function drawSky() {
 	
 	context.beginPath();
-	var sky = context.createLinearGradient(0, 0, 0, canvasDimensions.height/2);
+	var sky = context.createLinearGradient(0, 0, 0, canvas.height/2);
 	sky.addColorStop(0, "#3366ff");
 	sky.addColorStop(0.7, "#ffd1b3");
 	sky.addColorStop(1, "orange");
 	context.fillStyle = sky;
-	context.fillRect(0, 0, canvasDimensions.width, canvasDimensions.height*2/3);
+	context.fillRect(0, 0, canvas.width, canvas.height*2/3);
+	
 	
 }
 
@@ -84,8 +92,8 @@ function drawHorizon() {
 	}
 	
 	// Draw bottom corners
-	context.lineTo(canvasDimensions.width, canvasDimensions.height); // Bottom right
-	context.lineTo(0, canvasDimensions.height); // Bottom left
+	context.lineTo(canvas.width, canvas.height); // Bottom right
+	context.lineTo(0, canvas.height); // Bottom left
 	
 	context.closePath();
 	
@@ -99,6 +107,7 @@ function generateSegmentColor(horizonCount) {
     
 	// Generates a slowly darkening gray color hex string
 
+	//console.log("Horizon Count: " + horizonCount);
 	var value = 14 - horizonCount;
    
     color = "#" + value.toString(16) + value.toString(16) + value.toString(16);
@@ -107,39 +116,4 @@ function generateSegmentColor(horizonCount) {
 
     return color;
     
-}
-
-
-function drawTitle(titleText) {
-
-	document.getElementById("title").style.position = "absolute";
-	document.getElementById("title").style.left = "25%";
-	document.getElementById("title").style.width = "50%";
-	document.getElementById("title").style.top = "20%";
-	
-	document.getElementById('title').innerHTML = "<font face='Georgia' size='7' color=#333>" + titleText + "</font>";
-
-}
-
-function drawDescription(descriptionText) {
-	
-	document.getElementById("description").style.position = "absolute";
-	document.getElementById("description").style.left = "25%";
-	document.getElementById("description").style.width = "50%";
-	document.getElementById("description").style.top = "27%";
-	
-	document.getElementById('description').innerHTML = "<font face='Georgia' size='5' color=#555>" + descriptionText + "</font>";
-	
-}
-
-function drawStrap() {
-	
-	document.getElementById("footer").style.position = "absolute";
-	document.getElementById("footer").style.left = "25%";
-	document.getElementById("footer").style.width = "50%";
-	document.getElementById("footer").style.top = "35%";
-	
-	document.getElementById('footer').innerHTML = "<font face='Georgia' size='5' color=#555>Footer</font>";	
-	
-	
 }
